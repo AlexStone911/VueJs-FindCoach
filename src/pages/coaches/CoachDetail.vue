cda3326530b4936d92f3dc88e8de10e76c9c9d24
AlexStone911 <podgorodeczkij199@gmail.com>
2023-01-05 00:47:28 +0200

    Routes setup

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,4 @@
+<template>
+    Coach Details
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        Filter
+    </section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as coach</router-link>
+        </div>
+        List of coaches
+    </section>
+</template>
